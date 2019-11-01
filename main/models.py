1fa3bb2dcaf05d06b85a45d3f271651f3bee908e
MrEscape54 <martino.guzman@gmail.com>
2019-11-01 16:41:34 -0300

    Adding Product List Page

--- main/models.py
+++ main/models.py
@@ -1,10 +1,33 @@
 from django.db import models
 
+
+class ProductTagManager(models.Manager):
+   def get_by_natural_key(self, slug):
+      return self.get(slug=slug)
+
+
+class ProductTag(models.Model):
+   name = models.CharField(max_length=40)
+   slug = models.SlugField(max_length=48)
+   description = models.TextField(blank=True)
+   active = models.BooleanField(default=True)
+
+   objects = ProductTagManager()   
+   
+   def __str__(self):
+       return self.name
+       
+   def natural_key(self):
+      return (self.slug,)
+
+
 class ActiveManager(models.Manager):
    def active(self):
       return self.filter(active=True)
 
+
 class Product(models.Model):
+   tags = models.ManyToManyField(ProductTag, blank=True)
    name = models.CharField(max_length=40)
    description = models.TextField(blank=True)
    price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -13,19 +36,14 @@ class Product(models.Model):
    in_stock = models.BooleanField(default=True)
    date_updated = models.DateField(auto_now=True)
 
+   object = ActiveManager()
+
    def __str__(self):
       return self.name
    
-   object = ActiveManager()
 
 class ProductImage(models.Model):
    product = models.ForeignKey(Product, on_delete=models.CASCADE)
    image = models.ImageField(upload_to='product-images')
    thumbnail = models.ImageField(upload_to='product-thumbnails', null=True)
-
-class ProductTag(models.Model):
-   product = models.ManyToManyField(Product, blank=True)
-   name = models.CharField(max_length=40)
-   slug = models.SlugField(max_length=48)
-   description = models.TextField(blank=True)
-   active = models.BooleanField(default=True)
+   
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from main import views
+from main import models
 
 urlpatterns = [
     path('about/', TemplateView.as_view(template_name='about.html'), name='about',),
     path('', TemplateView.as_view(template_name='index.html'), name='index',),
     path('contact-us/', views.ContactUsView.as_view(), name='contact_us',),
+    path('products/<slug:tag>/', views.ListView.as_view(model=models.Product), name='products',),
 ]
